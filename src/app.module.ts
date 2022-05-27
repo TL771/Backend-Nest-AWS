95f912f026a8a2878d03c354448c98101890865c
6204062630084 <s6204062630084@email.kmutnb.ac.th>
2022-05-28 01:30:53 +0700

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { App2Service } from './app2/app2.service';
 
 @Module({
   imports: [AuthModule, UsersModule,TypeOrmModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, App2Service],
 })
 export class AppModule {
 }
